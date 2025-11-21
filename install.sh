d83acb57ff6ae37f091d6a9bbf9e29ac687207d6
adamsoliev <asoliyev13@gmail.com>
2025-11-21 11:34:25 -0600

    added vim-plug manager installation

--- install.sh
+++ install.sh
@@ -27,6 +27,15 @@ else
   echo "Oh My Zsh is already installed."
 fi
 
+# Install vim-plug (Vim plugin manager)
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+  echo "Installing vim-plug..."
+  curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+  echo "vim-plug is already installed."
+fi
+
 # Copy dotfiles from repo to home
 echo "Installing dotfiles..."
 cp .zshrc ~/
